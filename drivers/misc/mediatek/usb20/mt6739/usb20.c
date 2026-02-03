16139ee7b4f0344f4b715807cfd25981a32fdc61
karamdev1 <karamgaggi100@gmail.com>
2026-02-03 13:18:09 +0300

    Using musb.h instead of mtk_musb.h

--- drivers/misc/mediatek/usb20/mt6739/usb20.c
+++ drivers/misc/mediatek/usb20/mt6739/usb20.c
@@ -22,7 +22,8 @@
 #include "musbhsdma.h"
 #include "usb20.h"
 
-extern struct mtk_musb *g_musb;
+#include "musb.h"
+extern struct musb *g_musb;
 
 #include <mt-plat/mtk_boot_common.h>
 #include <mt-plat/charger_type.h>
