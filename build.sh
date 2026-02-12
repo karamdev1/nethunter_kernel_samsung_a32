514eed6afde69e7197786a9b5ef7a98c78277be8
karamdev1 <karamgaggi100@gmail.com>
2026-02-13 01:30:51 +0300

    New Kernel Builder

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ fi
 
 source config
 
-$CLANG_PATH=$(eval echo $CLANG_PATH)
+CLANG_PATH=$(eval echo $CLANG_PATH)
 export PATH="$CLANG_PATH:$PATH"
 export ARCH=$(eval echo $ARCH)
 export CC=$(eval echo $CC)
@@ -52,32 +52,23 @@ function show_gui() {
 	echo -e "${BOLDGREEN}Kernel Config: ${CONFIG_STATUS}$ENDCOLOR"
 	echo
 	echo -e "$BOLDGREEN|-----------------------------------------------------|$ENDCOLOR"
-	echo -e "${BOLDGREEN}| Actions:                                            |$ENDCOLOR"
-	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}Kernel${BOLDGREEN}----------------------------|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}1${ENDCOLOR}] Compile Kernel                           $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}2${ENDCOLOR}] Compile Module $BOLDBLUE(Doesn't need prepare)    $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}3${ENDCOLOR}] Prepare Module                           $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN| Actions:                                            |$ENDCOLOR"
+	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}Kernel$BOLDGREEN----------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}1$ENDCOLOR] Compile Kernel                           $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}2$ENDCOLOR] Compile Module $BOLDBLUE(Doesn't need prepare)    $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}3$ENDCOLOR] Prepare Module                           $BOLDGREEN|$ENDCOLOR"
 	echo -e "$BOLDGREEN|-----------------------------------------------------|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}4${ENDCOLOR}] Clean Kernel $BOLDBLUE(Clean & Mrproper)          $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}5${ENDCOLOR}] Apply Defconfig $BOLDBLUE(Selection in Config)    $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}4$ENDCOLOR] Clean Kernel $BOLDBLUE(Clean & Mrproper)          $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}5$ENDCOLOR] Apply Defconfig $BOLDBLUE(Selection in Config)    $BOLDGREEN|$ENDCOLOR"
 	echo -e "$BOLDGREEN|-----------------------------------------------------|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}6${ENDCOLOR}] Edit Config $BOLDBLUE(MENUCONFIG) $BOLDYELLOW(GOOD)          $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}7${ENDCOLOR}] Edit Config $BOLDBLUE(NCONFIG) $BOLDGREEN(BEST)             $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}8${ENDCOLOR}] Save .config as new defconfig            $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}Script${BOLDGREEN}----------------------------|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDRED}E${ENDCOLOR}] Exit Builder                             $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|        ${ENDCOLOR}[${BOLDBLUE}G${ENDCOLOR}] Open the creator's github page           $BOLDGREEN|$ENDCOLOR"
-	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}End${BOLDGREEN}-------------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}6$ENDCOLOR] Edit Config $BOLDBLUE(MENUCONFIG) $BOLDYELLOW(GOOD)          $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}7$ENDCOLOR] Edit Config $BOLDBLUE(NCONFIG) $BOLDGREEN(BEST)             $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}8$ENDCOLOR] Save .config as new defconfig            $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}Script$BOLDGREEN----------------------------|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDRED}E$ENDCOLOR] Exit Builder                             $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|        $ENDCOLOR[${BOLDBLUE}G$ENDCOLOR] Open the creator's github page           $BOLDGREEN|$ENDCOLOR"
+	echo -e "$BOLDGREEN|-------------------${ENDCOLOR}End$BOLDGREEN-------------------------------|$ENDCOLOR"
 	echo
-}
-
-function check_exit_code() {
-	local ret=$?
-	if [ $ret -eq 0 ]; then
-		echo -e "$BOLDGREEN[+] Success$ENDCOLOR"
-	else
-		echo -e "$BOLDRED[-] Failed (exit code: $ret)$ENDCOLOR"
-	fi
 }
 
 while true; do
@@ -92,20 +83,12 @@ while true; do
 				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
 			else
 				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
-				make -s -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j"$(nproc)" && make -C "$KDIR" O="$OUT_DIR" INSTALL_MOD_PATH="modules" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_install -j"$(nproc)"
-				check_exit_code
-				echo -e "$BOLDGREEN[+] You can find the modules in '$OUT_DIR/modules'$ENDCOLOR"
-				if [ ! -f "$KDIR/$OUT_DIR/arch/$ARCH/boot/Image" ]; then
-					echo -e "$BOLDRED[-] Image binary isn't made$ENDCOLOR"
+				make -s -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Kernel Building Succeed$ENDCOLOR"
 				else
-					echo -e "$BOLDGREEN[+] Image binary is being copied to '$KDIR/arch/$ARCH/boot/Image'$ENDCOLOR"
-					cp "$KDIR/$OUT_DIR/arch/$ARCH/boot/Image" "$KDIR/arch/$ARCH/boot/Image"
-					ret=$?
-					if [ $ret -eq 0 ]; then
-						echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
-					else
-						echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
-					fi
+					echo -e "$BOLDRED[-] Kernel Building Failed (exit code: $ret)$ENDCOLOR"
 				fi
 			fi
 			;;
@@ -115,49 +98,78 @@ while true; do
 				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
 			else
 				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
-				make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y module_prepare -j"$(nproc)" && make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y module -j"$(nproc)" && make -C "$KDIR" O="$OUT_DIR" INSTALL_MOD_PATH="modules" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_install -j"$(nproc)"
-				check_exit_code
-				echo -e "$BOLDGREEN[+] You can find the modules in '$OUT_DIR/modules'$ENDCOLOR"
+				make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_prepare -j"$(nproc)" && make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules -j"$(nproc)" && make -C "$KDIR" O="$OUT_DIR" INSTALL_MOD_PATH="modules" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_install -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Modules Building Succeed$ENDCOLOR"
+					echo -e "$BOLDGREEN[+] You can find the modules in '$OUT_DIR/modules'$ENDCOLOR"
+				else
+					echo -e "$BOLDRED[-] Modules Building Failed (exit code: $ret)$ENDCOLOR"
+				fi
 			fi
 			;;
-		2)
+		3)
 			echo -e "$BOLDGREEN[+] Preparing Modules$ENDCOLOR"
 			if [ ! -f "$KDIR/$OUT_DIR/.config" ]; then
 				echo -e "$BOLDRED[-] No .config found$ENDCOLOR"
 			else
 				echo -e "$BOLDGREEN[+] .config found$ENDCOLOR"
-				make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y module_prepare -j"$(nproc)"
-				check_exit_code
+				make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y modules_prepare -j"$(nproc)"
+				ret=$?
+				if [ $ret -eq 0 ]; then
+					echo -e "$BOLDGREEN[+] Preparing Modules Succeed$ENDCOLOR"
+				else
+					echo -e "$BOLDRED[-] Preparing Modules Failed (exit code: $ret)$ENDCOLOR"
+				fi
 			fi
 			;;
 		4)
 			echo -e "$BOLDGREEN[+] Cleaning$ENDCOLOR"
 			make -s -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean -j"$(nproc)" && make -s -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper -j"$(nproc)" && make -s -C "$KDIR" O="$KDIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y clean -j"$(nproc)" && make -s -C "$KDIR" O="$KDIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y mrproper -j"$(nproc)"
-			check_exit_code
+			ret=$?
+			if [ $ret -eq 0 ]; then
+				echo -e "$BOLDGREEN[+] Cleaning Succeed$ENDCOLOR"
+			else
+				echo -e "$BOLDRED[-] Cleaning Failed (exit code: $ret)$ENDCOLOR"
+			fi
 			;;
 		5)
-			echo -e "$BOLDGREEN[+] Appling $DEFCONFIG$ENDCOLOR"
+			echo -e "$BOLDGREEN[+] Applying $DEFCONFIG$ENDCOLOR"
 			if [ ! -f "$KDIR/arch/$ARCH/configs/$DEFCONFIG" ]; then
 				echo -e "$BOLDRED[-] $DEFCONFIG is not found$ENDCOLOR"
 			else
 				echo -e "$BOLDGREEN[+] $DEFCONFIG is found$ENDCOLOR"
 				echo -ne "$BOLDRED[!] Want to apply $BOLDYELLOW$DEFCONFIG$BOLDRED?$ENDCOLOR [Y,n]: "
 				read answer
-				if [[ "$answer" == "Y" || "$answer" == "y" || "$answer" == "" ]]; then
-					make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y $DEFCONFIG -j"$(nproc)"
-					check_exit_code
-				elif [[ "$answer" == "N" || "$answer" == "n" ]]; then
-					echo -ne "$BOLDGREEN[+] Enter the defconfig's name: $ENDCOLOR"
-					read config
-					if [ ! -f "$KDIR/arch/$ARCH/configs/$config" ]; then
-						echo -e "$BOLDRED[-] $config is not found$ENDCOLOR"
-					else
-						make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y $config -j"$(nproc)"
-						check_exit_code
-					fi
-				else
-					echo -e "$BOLDRED[!] Invalid Action!!$ENDCOLOR"
-				fi
+				case "$answer" in
+					Y|y|'')
+						make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y $DEFCONFIG -j"$(nproc)"
+						ret=$?
+						if [ $ret -eq 0 ]; then
+							echo -e "$BOLDGREEN[+] Config Applying Succeed$ENDCOLOR"
+						else
+							echo -e "$BOLDRED[-] Config Applying Failed (exit code: $ret)$ENDCOLOR"
+						fi
+						;;
+					N|n)
+						echo -ne "$BOLDGREEN[+] Enter the defconfig's name: $ENDCOLOR"
+						read config
+						if [ ! -f "$KDIR/arch/$ARCH/configs/$config" ]; then
+							echo -e "$BOLDRED[-] $config is not found$ENDCOLOR"
+						else
+							make -C "$KDIR" O="$OUT_DIR" KCFLAGS="$KCFLAGS" CONFIG_SECTION_MISMATCH_WARN_ONLY=y $config -j"$(nproc)"
+							ret=$?
+							if [ $ret -eq 0 ]; then
+								echo -e "$BOLDGREEN[+] Config Applying Succeed$ENDCOLOR"
+							else
+								echo -e "$BOLDRED[-] Config Applying Failed (exit code: $ret)$ENDCOLOR"
+							fi
+						fi
+						;;
+					*)
+						echo -e "$BOLDRED[!] Invalid Action!!$ENDCOLOR"
+						;;
+				esac
 			fi
 			;;
 		6)
@@ -179,25 +191,28 @@ while true; do
 					echo -e "$BOLDRED[!] $newconfig is found"
 					echo -ne "Do you want to overwrite it?$ENDCOLOR [y,N]: "
 					read answer
-					if [[ "$answer" == "Y" || "$answer" == "y" ]]; then
-						echo -e "$BOLDGREEN[+] Overwriting current .config as $newconfig$ENDCOLOR"
-						cp "$KDIR/$OUT_DIR/.config" "$KDIR/arch/$ARCH/configs/$newconfig"
-						ret=$?
-						if [ $ret -eq 0 ]; then
-							echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
-						else
-							echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
-						fi
-					fi
-				else
-					echo -e "$BOLDGREEN[+] Saving current .config as $newconfig$ENDCOLOR"
-					cp "$KDIR/$OUT_DIR/.config" "$KDIR/arch/$ARCH/configs/$newconfig"
-					ret=$?
-					if [ $ret -eq 0 ]; then
-						echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
-					else
-						echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
-					fi
+					case "$answer" in
+						Y|y)
+							echo -e "$BOLDGREEN[+] Overwriting current .config as $newconfig$ENDCOLOR"
+							cp "$KDIR/$OUT_DIR/.config" "$KDIR/arch/$ARCH/configs/$newconfig"
+							ret=$?
+							if [ $ret -eq 0 ]; then
+								echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
+							else
+								echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
+							fi
+							;;
+						N|n|'')
+							echo -e "$BOLDGREEN[+] Saving current .config as $newconfig$ENDCOLOR"
+							cp "$KDIR/$OUT_DIR/.config" "$KDIR/arch/$ARCH/configs/$newconfig"
+							ret=$?
+							if [ $ret -eq 0 ]; then
+								echo -e "$BOLDGREEN[+] Coping succeed$ENDCOLOR"
+							else
+								echo -e "$BOLDRED[-] Coping Failed (exit code: $ret)$ENDCOLOR"
+							fi
+							;;
+					esac
 				fi
 			fi
 			;;
