72deadf7037e8243218a7e7e1525bf5a6043e598
karamdev1 <karamgaggi100@gmail.com>
2026-02-23 12:54:37 +0300

    Revert "Patching ConfigFS UEVENT to add runtime toggle"
    This reverts commit afc4fef2d96594d3a12c273414b4c059739376fc.

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1549,8 +1549,6 @@ err_comp_cleanup:
 }
 
 #ifdef CONFIG_USB_CONFIGFS_UEVENT
-static bool enable_gadget_uevent = true;
-module_param(enable_gadget_uevent, bool, 0644);
 static void android_work(struct work_struct *data)
 {
 	struct gadget_info *gi = container_of(data, struct gadget_info, work);
@@ -1580,9 +1578,8 @@ static void android_work(struct work_struct *data)
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
 	if (status[0]) {
-		if (enable_gadget_uevent)
-			kobject_uevent_env(&android_device->kobj,
-						KOBJ_CHANGE, connected);
+		kobject_uevent_env(&android_device->kobj,
+					KOBJ_CHANGE, connected);
 		pr_info("%s: sent uevent %s\n", __func__, connected[0]);
 		uevent_sent = true;
 #ifdef CONFIG_USB_NOTIFY_PROC_LOG
@@ -1592,9 +1589,8 @@ static void android_work(struct work_struct *data)
 	}
 
 	if (status[1]) {
-		if (enable_gadget_uevent)
-			kobject_uevent_env(&android_device->kobj,
-						KOBJ_CHANGE, configured);
+		kobject_uevent_env(&android_device->kobj,
+					KOBJ_CHANGE, configured);
 		pr_info("%s: sent uevent %s\n", __func__, configured[0]);
 		uevent_sent = true;
 #ifdef CONFIG_USB_NOTIFY_PROC_LOG
@@ -1613,9 +1609,8 @@ static void android_work(struct work_struct *data)
 	}
 
 	if (status[2]) {
-		if (enable_gadget_uevent)
-			kobject_uevent_env(&android_device->kobj,
-						KOBJ_CHANGE, disconnected);
+		kobject_uevent_env(&android_device->kobj,
+					KOBJ_CHANGE, disconnected);
 		pr_info("%s: sent uevent %s\n", __func__, disconnected[0]);
 		uevent_sent = true;
 #ifdef CONFIG_USB_NOTIFY_PROC_LOG
